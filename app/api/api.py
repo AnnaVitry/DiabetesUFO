9f89839e4c5eee9d2fb5ffd99b1028f2e007ddbf
FidelMH <moussahaziri.fidel@gmail.com>
2025-10-10 10:51:06 +0400

    loading model

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+import joblib
+# from  sklearn.ensemble import RandomForestClassifier
 
+diabeast = joblib.load('model/diabeast.pkl')
+diabeast.predict()
 app = FastAPI()
 
 @app.get('/')
 def read_root():
     return {'message': 'Hello from root'}
 
+
+@app.post('/predict')
+def predict(**args):
+    resultat = ''
+    return {'result': resultat}
