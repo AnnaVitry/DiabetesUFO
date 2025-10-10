7ef22a914df992f426ffe7b754dacc615897a392
AnnaVitry <anne-marion119@hotmail.com>
2025-10-10 11:35:24 +0400

    modif api.py

--- app/api/api.py
+++ app/api/api.py
@@ -12,6 +12,7 @@ class PatientData(BaseModel):
     sudden_weight_loss: int
     weakness: int
     polyphagia: int
+    genital_thrush: int
     visual_blurring: int
     itching: int
     irritability: int
@@ -46,3 +47,24 @@ def predict(data:PatientData):
             "positive": proba[1]
         }
     }
+   
+# curl -X POST "http://127.0.0.1:8000/predict" \
+# -H "Content-Type: application/json" \
+# -d '{
+#   "age": 45,
+#   "gender": 1,
+#   "polyuria": 1,
+#   "polydipsia": 0,
+#   "sudden_weight_loss": 0,
+#   "weakness": 1,
+#   "polyphagia": 0,
+#   "genital_thrush": 1,
+#   "visual_blurring": 0,
+#   "itching": 0,
+#   "irritability": 1,
+#   "delayed_healing": 0,
+#   "partial_paresis": 0,
+#   "muscle_stiffness": 0,
+#   "alopecia": 0,
+#   "obesity": 1
+# }'
